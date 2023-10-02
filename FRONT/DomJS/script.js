2494678769fde34e3a767c51f8975d55a601cb78
unknown <mjgutierre@eafit.edu.co>
2023-10-01 21:26:50 -0500

    clase3 DOM

--- FRONT/DomJS/script.js
+++ FRONT/DomJS/script.js
@@ -1,28 +1,44 @@
-const btn = document.querySelector("[data-form-btn]");
+(() => {//para no estar al alcance del usuario
+  const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento) => {
-  //funcion anonima
-  evento.preventDefault(); //el objeto no recarga la pagina
-  const input = document.querySelector("[data-form-input]");
-  const value = input.value;
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
-  task.classList.add("card");
-  input.value = "";
-  const content = ` <div>
-  <i class="far fa-check-square icon"></i>
-  <span class="task">${value}</span>
-</div>
-<i class="fas fa-trash-alt trashIcon icon"></i>
-</li>`; //$codigo javascript en html
-  task.innerHTML = content;
+  const createTask = (evento) => {//el objeto no recarga la pagina
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    //backticks
+    const taskContent = document.createElement('div');
 
-  list.appendChild(task);
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    const content = `
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;//$codigo javascript en html
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
+  };
 
-  console.log(content);
-};
+  //Arrow functions o funciones anonimas
+  btn.addEventListener('click', createTask);
 
-console.log(btn);
+  const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);//click y la funcion al darle click
+    return i;
+  };
+  // Immediately invoked function expression IIFE
+  const completeTask = (event) => {
+    const element = event.target;//cambia un elemento icono para completado
+    element.classList.toggle('fas');//se puede tambien add y remove (verifica si existe la clase la quita sino la pone)
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+  };
+})();
 
-//Arrow functions o funciones anonimas
-btn.addEventListener("click", createTask);
