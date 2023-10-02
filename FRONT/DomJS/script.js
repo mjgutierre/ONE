d5c242e2f97c81d7366925d3e86cbc8eff285cf1
unknown <mjgutierre@eafit.edu.co>
2023-10-01 21:05:52 -0500

    clase2 DOM

--- FRONT/DomJS/script.js
+++ FRONT/DomJS/script.js
@@ -1,12 +1,28 @@
-const btn = document.querySelector('[data-form-btn]');
+const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (evento) => {//funcion anonima
-  evento.preventDefault();//el objeto no recarga la pagina 
-  const input = document.querySelector('[data-form-input]');
-  console.log(input.value);
+const createTask = (evento) => {
+  //funcion anonima
+  evento.preventDefault(); //el objeto no recarga la pagina
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+  const content = ` <div>
+  <i class="far fa-check-square icon"></i>
+  <span class="task">${value}</span>
+</div>
+<i class="fas fa-trash-alt trashIcon icon"></i>
+</li>`; //$codigo javascript en html
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
+  console.log(content);
 };
 
 console.log(btn);
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener("click", createTask);
